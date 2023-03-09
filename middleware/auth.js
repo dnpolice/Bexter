7ca555ef21929137cfcedef883ab73070a2bc4cc
Dylan Policelli <dylanpolicelli@gmail.com>
2023-03-09 00:12:44 -0500

    temp fix for error decoding jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,25 +2,22 @@ const jwt = require('jsonwebtoken');
 const userQuery = require('../routes/users');
 
 module.exports = async (req, res, next) => {
-    /*
-    const token = req.headers['x-auth-token']
-    // to use ^, where is the token stored..? - in req.body.token? or just a var?
-    if (token == null) return res.sendStatus(401)
-    req.user = token
-    next()
-    // OR below
-    */
-
     if (req.cookies.saveUser) {
-        if (req.cookies.saveUser == 'loggedout'){
+        if (req.cookies.saveUser == 'loggedout') {
             return res.status(401).json({msg: 'not logged in'});    
         }
+        console.log(req.cookies.saveUser);
         const decoded = jwt.verify(
             req.cookies.saveUser, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                console.log(decoded)
                 if (err) console.log(err)
-                else return decoded
+                return decoded;
             }
-        )   
+        )
+        if (decoded == null) {
+            return res.status(401).json({msg: 'error decoding'});
+        }
+        
         const getUser = () => { return new Promise(resolve =>
             userQuery.getUserWithEmail(decoded.email, (err, results) => {
                 if (err) console.log(err)
@@ -31,8 +28,10 @@ module.exports = async (req, res, next) => {
         if (!theUser) console.log('user email not found');        
         
         req.user = theUser[0];
+
+        next();
     } else {
         return res.status(401).json({msg: 'not logged in'});    
     }    
-   next();
+
 }
